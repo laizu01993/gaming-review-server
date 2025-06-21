b90d9cd379aada4c1a16c1b27461741155a45e6c
laizu01993 <laizuaktar6@gmail.com>
2025-06-22 03:06:12 +0600

    created add review from clientside and get data

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tye2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,6 +30,18 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const reviewCollection = client.db('reviewDB').collection('review')
+        // to get review data in mongodb
+        app.post('/review', async(req, res)=>{
+            const newReview = req.body;
+            console.log(newReview);
+            const result = await reviewCollection.insertOne(newReview);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
